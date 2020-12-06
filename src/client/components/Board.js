02e55d7177e116a354ff1f26327dd8f60b6958f0
Daniel Jin <danieljin123@outlook.com>
2020-12-07 01:36:22 +0300

    Board.js formatted, stylying moved to styld-component

--- src/client/components/Board.js
+++ src/client/components/Board.js
@@ -1,63 +1,79 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { Link } from 'react-router-dom'
-import GAME_CONFIG from '../../../GAME_CONFIG.json';
-import React, { useContext, useState, useEffect, useLayoutEffect, useCallback } from 'react';
-import styled from 'styled-components';
-import { BoardCard } from './BoardCard';
-import { store } from '../store/Store';
-const FIELD_SIZE = GAME_CONFIG.maxCardsInField;
+import GAME_CONFIG from '../../../GAME_CONFIG.json'
+import React, {
+	useContext,
+	useState,
+	useEffect,
+	useLayoutEffect,
+	useCallback,
+} from 'react'
+import styled from 'styled-components'
+import { BoardCard } from './BoardCard'
+import { store } from '../store/Store'
+
+const FIELD_SIZE = GAME_CONFIG.maxCardsInField
 const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-};
+	const result = Array.from(list)
+	const [removed] = result.splice(startIndex, 1)
+	result.splice(endIndex, 0, removed)
+	return result
+}
 
 /**
-*@param {source} - source 
-*@param {destination} - destination
-*@param {droppableSource} - source 
-*@param {droppableDestination} - destination
-**/
+ *@param {source} - source
+ *@param {destination} - destination
+ *@param {droppableSource} - source
+ *@param {droppableDestination} - destination
+ **/
 const move = (source, destination, droppableSource, droppableDestination) => {
-	const sourceClone = Array.from(source);
-	const destClone = Array.from(destination);
-	const [removed] = sourceClone.splice(droppableSource.index, 1);
+	const sourceClone = Array.from(source)
+	const destClone = Array.from(destination)
+	const [removed] = sourceClone.splice(droppableSource.index, 1)
 
-	destClone.splice(droppableDestination.index, 0, removed);
+	destClone.splice(droppableDestination.index, 0, removed)
 
-	const result = {};
-	result[droppableSource.droppableId] = sourceClone;
-	result[droppableDestination.droppableId] = destClone;
+	const result = {}
+	result[droppableSource.droppableId] = sourceClone
+	result[droppableDestination.droppableId] = destClone
 
-	return result;
-};
+	return result
+}
 
 const ALLY_TYPES = {
 	hand: 'allyHand',
-	field: 'allyField'
+	field: 'allyField',
 }
 
 export const Board = (props) => {
 	const { state, dispatch } = useContext(store)
 	const isGamePaused = () => state.game && state.game.gamePaused
-    const [allyCards, setAllyCards] = useState({ [ALLY_TYPES.hand]: [], [ALLY_TYPES.field]: [] })
+	const [allyCards, setAllyCards] = useState({
+		[ALLY_TYPES.hand]: [],
+		[ALLY_TYPES.field]: [],
+	})
 
 	useLayoutEffect(() => {
-        if(state.game) {
-            if(state.playerNumber === 1) {
-                setAllyCards({[ALLY_TYPES.hand]: [...state.game.player1.hand], [ALLY_TYPES.field]: [...state.game.player1.field]})
-            } else {
-                setAllyCards({[ALLY_TYPES.hand]: [...state.game.player2.hand], [ALLY_TYPES.field]: [...state.game.player2.field]})
-            }
-        }
-	}, [state.game]);
+		if (state.game) {
+			if (state.playerNumber === 1) {
+				setAllyCards({
+					[ALLY_TYPES.hand]: [...state.game.player1.hand],
+					[ALLY_TYPES.field]: [...state.game.player1.field],
+				})
+			} else {
+				setAllyCards({
+					[ALLY_TYPES.hand]: [...state.game.player2.hand],
+					[ALLY_TYPES.field]: [...state.game.player2.field],
+				})
+			}
+		}
+	}, [state.game])
 
 	const getAllyStateType = (droppableId) => {
 		return allyCards[droppableId]
-    }
+	}
 
-    const invokeCard = (card) => {
+	const invokeCard = (card) => {
 		console.log('invoke card', card)
 		if (!card.isInvoked) {
 			let me
@@ -90,44 +106,65 @@ export const Board = (props) => {
 				card,
 			})
 		}
-    }
+	}
 
-	const onDragEnd = useCallback((result) => {
-		const { source, destination } = result;
-		if (!destination) {
-            return;
-        }
-        let allyState = getAllyStateType(source.droppableId);
-        const isEnoughEnergyToInvoke = state.playerNumber === 1 ? state.game.player1.energy : state.game.player2.energy;
-        console.log(isEnoughEnergyToInvoke, allyState.cost, 'energy', state.playerNumber, state.game.player1.energy, state.game.player2.energy)
-        if(isEnoughEnergyToInvoke < allyState[source.index].cost) {
-            return;
-        }
-		if (source.droppableId === destination.droppableId) {
-            const items = reorder(
-                allyState,
-                source.index,
-                destination.index
-            );
-            setAllyCards({...allyCards, [source.droppableId]: items})
-		} else {
-            //invoke card
-            invokeCard(allyCards[ALLY_TYPES.hand][source.index]);
-			const result = move(getAllyStateType(source.droppableId), getAllyStateType(destination.droppableId), source, destination);
-            setAllyCards({[ALLY_TYPES.hand]: result[ALLY_TYPES.hand], [ALLY_TYPES.field]: result[ALLY_TYPES.field]});
-		}
-    }, [state.game, allyCards]);
+	const onDragEnd = useCallback(
+		(result) => {
+			const { source, destination } = result
+			if (!destination) {
+				return
+			}
+			let allyState = getAllyStateType(source.droppableId)
+			const isEnoughEnergyToInvoke =
+				state.playerNumber === 1
+					? state.game.player1.energy
+					: state.game.player2.energy
+			console.log(
+				isEnoughEnergyToInvoke,
+				allyState.cost,
+				'energy',
+				state.playerNumber,
+				state.game.player1.energy,
+				state.game.player2.energy,
+			)
+			if (isEnoughEnergyToInvoke < allyState[source.index].cost) {
+				return
+			}
+			if (source.droppableId === destination.droppableId) {
+				const items = reorder(
+					allyState,
+					source.index,
+					destination.index,
+				)
+				setAllyCards({ ...allyCards, [source.droppableId]: items })
+			} else {
+				//invoke card
+				invokeCard(allyCards[ALLY_TYPES.hand][source.index])
+				const result = move(
+					getAllyStateType(source.droppableId),
+					getAllyStateType(destination.droppableId),
+					source,
+					destination,
+				)
+				setAllyCards({
+					[ALLY_TYPES.hand]: result[ALLY_TYPES.hand],
+					[ALLY_TYPES.field]: result[ALLY_TYPES.field],
+				})
+			}
+		},
+		[state.game, allyCards],
+	)
 
-	const getListStyle = isDraggingOver => ({
+	const getListStyle = (isDraggingOver) => ({
 		background: isDraggingOver ? 'lightblue' : 'lightgrey',
 		display: 'flex',
 		flexDirection: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100%',
 		overflow: 'auto',
-	  });
-	  
+	})
+
 	return (
 		<Page>
 			<ResultMsg
@@ -189,8 +226,8 @@ export const Board = (props) => {
 					<CardContainer className="cards-container enemy-cards-container">
 						{state.visualEnemyHand}
 					</CardContainer>
-                    <Field className="field">
-					<DragDropContext onDragEnd={onDragEnd}>
+					<Field className="field">
+						<DragDropContext onDragEnd={onDragEnd}>
 							<EnemyField
 								className={
 									state.isAttackMode
@@ -200,70 +237,100 @@ export const Board = (props) => {
 							>
 								{state.enemyFieldHtml}
 							</EnemyField>
-							<FieldContainer width='width: 70%;'>
-								<Droppable droppableId={`${ALLY_TYPES.field}`} direction="horizontal" >
+							<FieldContainer width="width: 70%;">
+								<Droppable
+									droppableId={`${ALLY_TYPES.field}`}
+									direction="horizontal"
+								>
 									{(provided, snapshot) => (
-                                        <div 
-                                            ref={provided.innerRef}
-										    style={getListStyle(snapshot.isDraggingOver)}
-                                            >
-                                            {allyCards[ALLY_TYPES.field].map((allyFieldCard, index) => (
-                                                    <Draggable
-                                                        key={index}
-                                                        draggableId={`allyFieldCard${index}`}
-                                                        index={index}
-                                                        isDragDisabled={true}>
-                                                            {(provided, snapshot)=> (
-                                                                <div
-                                                                    ref={provided.innerRef}
-                                                                    {...provided.draggableProps}
-                                                                    {...provided.dragHandleProps}
-                                                                >
-                                                                    <BoardCard
-                                                                        {...allyFieldCard}
-                                                                    />
-                                                                </div>
-                                                            )}
-                                                        </Draggable>
-                                            ))}
-										    {provided.placeholder}
+										<div
+											ref={provided.innerRef}
+											style={getListStyle(
+												snapshot.isDraggingOver,
+											)}
+										>
+											{allyCards[ALLY_TYPES.field].map(
+												(allyFieldCard, index) => (
+													<Draggable
+														key={index}
+														draggableId={`allyFieldCard${index}`}
+														index={index}
+														isDragDisabled={true}
+													>
+														{(
+															provided,
+															snapshot,
+														) => (
+															<div
+																ref={
+																	provided.innerRef
+																}
+																{...provided.draggableProps}
+																{...provided.dragHandleProps}
+															>
+																<BoardCard
+																	{...allyFieldCard}
+																/>
+															</div>
+														)}
+													</Draggable>
+												),
+											)}
+											{provided.placeholder}
 										</div>
 									)}
 								</Droppable>
 							</FieldContainer>
-                        <FieldContainer width='width: 70%;'>
-							<Droppable droppableId={`${ALLY_TYPES.hand}`} direction="horizontal">
-								{(provided, snapshot) => (
-									<div ref={provided.innerRef}
-										style={getListStyle(snapshot.isDraggingOver)}
-									>
-									{allyCards[ALLY_TYPES.hand].map((allyHandCard, index) => (
-											<Draggable
-												key={index}
-												draggableId={`allyHand${index}`}
-												index={index}
-                                                isDragDisabled={state.playerNumber !== state.game.currentPlayerTurn}
-                                            >
-													{(provided, snapshot)=> (
-														<div
-															ref={provided.innerRef}
-															{...provided.draggableProps}
-															{...provided.dragHandleProps}
-														>
-															<BoardCard
-																{...allyHandCard}
-															/>
-														</div>
-													)}
-											</Draggable>											
-									))}
-									{provided.placeholder}
-									</div>
-								)}									
-							</Droppable>
-                            </FieldContainer>
-					</DragDropContext>
-                    </Field>
+							<FieldContainer width="width: 70%;">
+								<Droppable
+									droppableId={`${ALLY_TYPES.hand}`}
+									direction="horizontal"
+								>
+									{(provided, snapshot) => (
+										<div
+											ref={provided.innerRef}
+											style={getListStyle(
+												snapshot.isDraggingOver,
+											)}
+										>
+											{allyCards[ALLY_TYPES.hand].map(
+												(allyHandCard, index) => (
+													<Draggable
+														key={index}
+														draggableId={`allyHand${index}`}
+														index={index}
+														isDragDisabled={
+															state.playerNumber !==
+															state.game
+																.currentPlayerTurn
+														}
+													>
+														{(
+															provided,
+															snapshot,
+														) => (
+															<div
+																ref={
+																	provided.innerRef
+																}
+																{...provided.draggableProps}
+																{...provided.dragHandleProps}
+															>
+																<BoardCard
+																	{...allyHandCard}
+																/>
+															</div>
+														)}
+													</Draggable>
+												),
+											)}
+											{provided.placeholder}
+										</div>
+									)}
+								</Droppable>
+							</FieldContainer>
+						</DragDropContext>
+					</Field>
 					<Button
 						className="end-turn"
 						disabled={state.isOtherPlayerTurn || isGamePaused()}
@@ -274,17 +341,17 @@ export const Board = (props) => {
 						End Turn
 					</Button>
 					<Button
-							className="end-turn"
-							style={{
-								backgroundColor: 'red',
-								marginTop: '10px',
-							}}
-							disabled={isGamePaused()}
-							onClick={() => {
-								props.surrender()
-							}}
-						>
-							Surrender
+						className="end-turn"
+						style={{
+							backgroundColor: 'red',
+							marginTop: '10px',
+						}}
+						disabled={isGamePaused()}
+						onClick={() => {
+							props.surrender()
+						}}
+					>
+						Surrender
 					</Button>
 				</Game>
 			) : (
@@ -324,10 +391,10 @@ const CardContainer = styled.div`
 	max-width: 70%;
 `
 const FieldContainer = styled.div`
-    background-color: lightgrey;
-    ${props => props.width}
-    height: 150px;
-    margin: 20px auto;
+	background-color: lightgrey;
+	${(props) => props.width}
+	height: 150px;
+	margin: 20px auto;
 `
 const StatsBox = styled.div`
 	position: absolute;

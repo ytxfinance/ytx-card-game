ad97e6371c393c2f7bb0e0a7e61b88d306b804ec
Phillip Musiime <phillipmusiime@gmail.com>
2020-12-27 09:48:22 +0300

    add styles for gameList

--- src/client/components/GameList.js
+++ src/client/components/GameList.js
@@ -21,45 +21,65 @@ export default () => {
 	return (
 		<GameListContainer>
 			{!state.gameList || state.gameList.length == 0 ? (
-				<p>No games available yet...</p>
+				<p>NO GAMES AVAILABLE YET...</p>
 			) : (
-				state.gameList.map((item) => (
-					<GameListItem key={item.gameId}>
-						<b>{item.gameName}</b>
-						<span>{item.ytxBet} YTX</span>
-						<Button
-							type="button"
-							onClick={() => joinGame(item.gameId)}
-						>
-							Join
+					state.gameList.map((item) => (
+						<GameListItem key={item.gameId}>
+							<b>{item.gameName.length > 10 ? item.gameName.substring(0, 10) + '...' : item.gameName}</b>
+							<span>{item.ytxBet} YTX</span>
+							<Button
+								type="button"
+								onClick={() => joinGame(item.gameId)}
+							>
+								JOIN
 						</Button>
-					</GameListItem>
-				))
-			)}
+						</GameListItem>
+					))
+				)}
 		</GameListContainer>
 	)
 }
 
 const GameListContainer = styled.ul`
-	border: 1px solid grey;
+	width: 28%;
+	margin-top:20px;
+	padding:0;
 	list-style-type: none;
+	@media(max-width: 860px){
+		width: 90%;
+	}
+	p {
+		font-size: 12px;
+		letter-spacing: 1px;
+		white-space: nowrap;
+		overflow: hidden;
+	}
 `
 const GameListItem = styled.li`
+	color: #000;
+	border-radius: 0.5rem;
+	/* margin: 0 auto; */
+	padding-left: 1rem;
+	background-color: #fff;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	height: 50px;
 `
 
 const Button = styled.button`
-	background-color: rgb(42, 90, 162);
-	border: none;
-	border-radius: 10px;
-	padding: 20px;
-	color: white;
+	background-color: #ff8a32;
+	border: transparent;
+	border-radius: 0.5rem;
+	color: #000;
+	font-size: 12px;
+	font-weight: 700;
 	cursor: pointer;
 	display: inline-block;
 	text-decoration: none;
-	min-width: 200px;
+	letter-spacing: 1px;
+	width: 30%;
+	height: 100%;
 
 	&:hover {
 		opacity: 0.7;

f75655ea45c60aa710e745bd771f7d6bd9b80a1f
Phillip Musiime <phillipmusiime@gmail.com>
2020-12-27 09:30:47 +0300

    add styles for createPage

--- src/client/pages/CreatePage.js
+++ src/client/pages/CreatePage.js
@@ -48,7 +48,7 @@ export default () => {
 
 	return (
 		<Page>
-			<h1>Create a game</h1>
+			<h1>CREATE A GAME</h1>
 			<LinkContainer hidden={!isWaiting}>
 				<b className={isWaiting ? '' : 'hidden'}>
 					The game will start automatically as soon as someone joins
@@ -64,48 +64,61 @@ export default () => {
 			</LinkContainer>
 			<LinkContainer hidden={isWaiting}>
 				<p>
-					Set how many YTX you want to bet for the game, press the
+					Set how many YTX you want to bet for the game, <br /> press the
 					button below to create a game.
 				</p>
-				<Input
-					type="text"
-					placeholder="Game name..."
-					onChange={(e) => {
-						setGameName(e.target.value)
-					}}
-				/>
-				<Input
-					type="number"
-					placeholder="Your YTX bet..."
-					onChange={(e) => {
-						dispatch({
-							type: 'SET_YTX_BET',
-							payload: {
-								ytxBet: e.target.value,
-							},
-						})
-					}}
-				/>
+				<InputWrapper>
+					<Input
+						type="text"
+						placeholder="Game name..."
+						onChange={(e) => {
+							setGameName(e.target.value)
+						}}
+					/>
+					<Input
+						type="number"
+						placeholder="Your YTX bet..."
+						onChange={(e) => {
+							dispatch({
+								type: 'SET_YTX_BET',
+								payload: {
+									ytxBet: e.target.value,
+								},
+							})
+						}}
+					/>
+				</InputWrapper>
 				<BoxyLink type="button" onClick={() => createGame()}>
 					Create
 				</BoxyLink>
 			</LinkContainer>
+			<HowToPlay>How to play?</HowToPlay>
 		</Page>
 	)
 }
 
 const Page = styled.div`
-	box-shadow: 0 0 30px 0 lightgrey;
+	color: #fff;
 	padding: 50px;
 	border-radius: 10px;
+	background-color: #1f1f1f;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	text-align: center;
 	margin: 0 auto;
-	min-width: 250px;
-	max-width: 550px;
+	width: 80%;
+	height: 80%;
 
 	h1 {
-		margin-top: 0;
+		margin: 0;
 	}
+	@media(max-width:425px){
+	width: 90%;
+	height: 85%;
+	padding:20px;
+}
 `
 const Button = styled.button`
 	background-color: rgb(42, 90, 162);
@@ -133,29 +146,45 @@ const Button = styled.button`
 	}
 `
 const BoxyLink = styled(Button)`
-	color: white;
+	color: #000;
 	text-decoration: none;
-	padding: 20px 0;
+	padding: 20px 10rem;
 	width: 100%;
+	border-radius: 0.5rem;
 	text-align: center;
-	background-color: #444444;
-
+	background-color: #ff8a32;
+	letter-spacing:1px;
+	font-size: 12px;
+	font-weight: 700;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	white-space: nowrap;
 	&:hover {
 		background-color: #2e2e2e;
+		color: #fff;
 	}
 
 	&:active {
 		color: whitesmoke;
 		background-color: #000000;
 	}
+	@media(max-width:560px){
+		padding: 20px 5rem;
+	}
 `
 const LinkContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	display: ${(props) => (props.hidden ? 'none' : 'block')};
+	p {
+		padding: 0 3rem 2rem;
+	}
 
 	a:not(:last-child) {
 		margin-bottom: 10px;
+	}
+	@media(max-width:768px){
+		width: 100%;
 	}
 `
 const Input = styled.input`
@@ -164,6 +193,34 @@ const Input = styled.input`
 	font-size: 15pt;
 	border: 1px solid lightgrey;
 	width: 100%;
-	box-sizing: border-box;
+	outline: none;
 	margin-bottom: 20px;
+`
+const InputWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	Input:first-child {
+		margin-right: 10px;
+		flex: 3;
+		@media(max-width:768px){
+			margin-right: 0;
+		}
+	}
+	Input:last-child{
+		flex: 1;
+	}
+	@media(max-width:768px){
+		flex-direction: column;
+		width: 100%;
+	}
+`
+
+const HowToPlay = styled.a`
+	color: #fff;
+	font-weight: 900;
+	font-size: 14px;
+	text-decoration-color: #fff;
+	text-decoration: underline;
+	padding-top: 3rem;
 `
